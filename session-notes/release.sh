46ad9e31a6372b4dbdcf67e8a2543484969142db
elliscode <45629640+elliscode@users.noreply.github.com>
2025-11-19 00:17:11 -0500

    Fixed the release script to sit removes files no longer present

--- session-notes/release.sh
+++ session-notes/release.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://daniel-townsend-dnd-notes-userspace/session-notes/ --exclude "*" --include "*.md"
+aws s3 sync . s3://daniel-townsend-dnd-notes-userspace/session-notes/ --exclude "*" --include "*.md" --delete
